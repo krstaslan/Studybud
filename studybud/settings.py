65162431d480b7da8d689cd17f73d146cbd09431
krstaslan <krstaslan34@gmail.com>
2022-06-07 13:15:15 +0200

    Automatic configuration with django-heroku

--- studybud/settings.py
+++ studybud/settings.py
@@ -143,3 +143,5 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS =True
+import django_heroku
+django_heroku.settings(locals())
