7cd4a21f0d1c3eb8198fa667d46390454d3e7935
Kursat Aslan <63463164+krstaslan@users.noreply.github.com>
2022-08-03 09:47:39 +0200

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['devhope.herokuapp.com']
+ALLOWED_HOSTS = ['*',devhope.herokuapp.com']
 
 CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
